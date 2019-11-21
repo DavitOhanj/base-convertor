b53d049255d52e47cd46e2303447ee67ec375105
DavitOhanj <57683628+DavitOhanj@users.noreply.github.com>
2019-11-21 23:28:33 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const reader = require('readline-sync');
 const utils = require('./converter');
 
-const num = reader.questionInt("Please enter the number you want to convert = ");
-const opt = reader.questionInt("Please enter option: \n 1:Binary \n 2:Hexadecimal =");
+const num = reader.questionInt("Please enter a number = ");
+const opt = reader.questionInt("Please choose an option: \n 1:Binary \n 2:Hexadecimal =");
 
 if(opt === 1){
   console.log(utils.convertToBinary(num));
 }else if(opt === 2){
   console.log(utils.convertToHexa(num));
 }else if(typeof opt === "number"){
-  console.log("There is no options for that number! ");
+  console.log("There are no options! ");
 }else{
-  console.log("Imported value should be number");
+  console.log("Import a number!");
 }
